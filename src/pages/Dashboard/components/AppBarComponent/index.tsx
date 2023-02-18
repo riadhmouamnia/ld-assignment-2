fc76c5df1d34046e99f752b51a1a235e550cb97b
Riadh mouamnia <riad.sodmg@gmail.com>
2023-02-18 22:54:21 +0100

    added current path to the AppBar component

--- src/pages/Dashboard/components/AppBarComponent/index.tsx
+++ src/pages/Dashboard/components/AppBarComponent/index.tsx
@@ -1,14 +1,17 @@
 import { Box, Toolbar, Typography } from "@mui/material";
-import { AppBar, AppBarProps } from "./styles/AppBarStyles";
+import { AppBar, AppBarProps, pathStyles } from "./styles/AppBarStyles";
 import stringAvatar from "./stringAvatar";
 import Events from "../../../../assets/Events.svg";
 import Bell from "../../../../assets/Bell.svg";
 import Avatar from "@mui/material/Avatar";
 
 import { useAppSelector } from "../../../../redux/hooks";
+import { useParams } from "react-router-dom";
+import { ChevronRight } from "components/icons";
 
 function AppBarComponent({ open }: AppBarProps) {
   const { userInfo } = useAppSelector(({ auth }) => auth);
+  const { id } = useParams();
   return (
     <AppBar open={open} elevation={0}>
       <Toolbar>
@@ -18,9 +21,22 @@ function AppBarComponent({ open }: AppBarProps) {
           alignItems="center"
           justifyContent="space-between"
         >
-          <Typography variant="h6" fontWeight="bold" color="text.primary">
-            Process
-          </Typography>
+          {id ? (
+            <Box sx={pathStyles}>
+              <Typography variant="h6" sx={pathStyles.greyText}>
+                Process
+              </Typography>
+              <ChevronRight />
+              <Typography variant="h6" sx={pathStyles.primaryText}>
+                Process {id}
+              </Typography>
+            </Box>
+          ) : (
+            <Typography variant="h6" sx={pathStyles.primaryText}>
+              Process
+            </Typography>
+          )}
+
           <Box display="flex" alignItems="center" gap={2}>
             <Box component="img" src={Bell} width="20px" height="20px" />
             <Box component="img" src={Events} width="26px" height="26px" />
