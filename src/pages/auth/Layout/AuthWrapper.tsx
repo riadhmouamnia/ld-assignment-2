95197a8e3303352afb2b8281fced7f3e1dce871e
Riadh mouamnia <riad.sodmg@gmail.com>
2023-02-20 19:39:26 +0100

    edit AuthWrapper to not have inline styling

--- src/pages/auth/Layout/AuthWrapper.tsx
+++ src/pages/auth/Layout/AuthWrapper.tsx
@@ -4,6 +4,7 @@ import Paper from "@mui/material/Paper";
 import logo from "assets/logo.svg";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "redux/hooks";
+import styled from "@emotion/styled";
 
 type AuthWrapperProps = {
   title: String;
@@ -11,6 +12,17 @@ type AuthWrapperProps = {
   children: any;
   cover: React.ReactNode;
 };
+
+const FormContainer = styled(Box)({
+  margin: "16px auto",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: 4,
+  width: "70%",
+});
+
+const FormWrapperStyles = { display: "flex", alignItems: "center" };
 
 function AuthWrapper({ children, cover, title, subTitle }: AuthWrapperProps) {
   const { userInfo } = useAppSelector(({ auth }) => auth);
@@ -24,18 +36,16 @@ function AuthWrapper({ children, cover, title, subTitle }: AuthWrapperProps) {
 
   return (
     <Grid container component="main" sx={{ height: "100vh" }}>
-      <Grid item px={3} xs={12} sm={8} md={5} component={Paper} square>
-        <Box
-          sx={{
-            my: 8,
-            mx: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: 4,
-            width: "70%",
-          }}
-        >
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        component={Paper}
+        sx={FormWrapperStyles}
+        square
+      >
+        <FormContainer>
           <Box width="100%">
             <img src={logo} alt="logo" width="44px" />
           </Box>
@@ -53,7 +63,7 @@ function AuthWrapper({ children, cover, title, subTitle }: AuthWrapperProps) {
             </Typography>
           </Box>
           {children}
-        </Box>
+        </FormContainer>
       </Grid>
       <Grid item xs={false} sm={4} md={7}>
         {cover}
